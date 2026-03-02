9a338bd97360801298a130e66197a532bfd2ee64
MarioW <mawhaley55@gmail.com>
2026-03-01 19:32:04 -0500

    Update buildS3Complete.bat

--- buildS3Complete.bat
+++ buildS3Complete.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 
-REM // This file has been gutted and replaced with the Lua build script.
-REM // It has been kept around for ease-of-use for Windows users.
+REM // This file is for ease-of-use for Windows users.
+REM // Get ready, because Sonic the Hedgehog's Huge Island Adventure will be built in a moment!
 "build_tools/Lua/lua.exe" buildS3Complete.lua || pause REM // Pause on Lua parse failure so that the user can read the error message.
